b213bfd05d0d6dfbe1c4c6e50d84cae778791561
pulseneon <thebooom@yandex.ru>
2024-02-07 01:50:54 +0300

    Добавьте файлы проекта.

---
+++ SkinsTopUp.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SkinsTopUp.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
